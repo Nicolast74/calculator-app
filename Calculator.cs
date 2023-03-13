3b1cf8cffdb7428c50c86350181e67e1d5f4c57b
Nicolast74 <nicolast.nicolast.74@gmail.com>
2023-03-13 16:56:20 +0700

    done

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,22 @@ class Calculator
 {
 	static void Main (string[] args)
 	{
+	int a = 10;
+	int b = 6;
 	
+	Console.WriteLine("Hasil Penambahan  : {0} + {1} = {2}", a, b, Penambahan(a,b));
+	Console.WriteLine("Hasil Pengurangan : {0} - {1} = {2}", a, b, Pengurangan(a,b));
+	
+	Console.WriteLine("\nTekan sembarang key untuk keluar");
+	Console.ReadKey();
+	}
+	
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
